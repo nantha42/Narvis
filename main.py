afbfc2bf65533925958a91c04e25ec8297e3025d
nantha42 <krnantha13@gmail.com>
2021-10-22 22:19:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from src import *
+from src.model import *
+
+
+if __name__ == '__main__':
+    t = Model()
---
+++ src/model.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
