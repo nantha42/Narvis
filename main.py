303729013ccd8ba5968b0624a5e1caea65624d46
nantha42 <krnantha13@gmail.com>
2021-10-29 23:04:22 +0530

    added some code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src import *
 from src.model import *
 
-
 if __name__ == '__main__':
     t = Model()
--- src/tutorial.py
+++ src/tutorial.py
@@ -233,5 +233,25 @@ pairs = trimRareWords(voc,pairs,MIN_COUNT)
 def indexesFromSentence(voc, sentence):
     return [voc.word2index[word] for word in sentence.split(' ')] + [EOS_token]
 
+def zeroPadding(l, fillvalue=PAD_token):
+    return list(itertools.zip_longes(*l, fillvalue=fillvalue))
+
+def binaryMatrix(l, value=PAD_token):
+    m = []
+    for i , seq in enumerate(l):
+        m.append([])
+        for token in seq:
+            if token == PAD_token:
+                m[i].append(0)
+            else:
+                m[i].append(1)
+    return m
+
+def inputVar(l, voc):
+    indexes_batc = [indexesFromSentence(voc, sentence) for sentence in l]
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    padVar = torch.LongTensor(padList)
+    return padVar, lengths
 
 
